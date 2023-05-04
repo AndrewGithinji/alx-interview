71679b482989d19749ee1846d0e54e9e9c0b5ae8
AndrewGithinji <andrewgithinji@hotmail.com>
2023-05-04 20:25:35 +0300

    Lockboxes Fixes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,28 +1,28 @@
 #!/usr/bin/python3
-"""Contains "canUnlockAll" function"""
+"""
+Module that contains the canUnlockAll function
+"""
+
 
 def canUnlockAll(boxes):
     """
-    Determine if all the boxes can be opened.
-
-    Prototype: def canUnlockAll(boxes)
-    boxes is a list of lists
-    A key with the same number as a box opens that box
-    You can assume all keys will be positive integers
-    There can be keys that do not have boxes
-    The first box boxes[0] is unlocked
-    Return True if all boxes can be opened, else return False
+    Determines if all the boxes can be opened.
     """
-    if type(boxes) is not list: return False
-    if len(boxes) == 0: return False
+    if type(boxes) is not list:
+        return False
+
+    if len(boxes) == 0:
+        return False
 
     keysbox = [0]
+
     for key in keysbox:
         for j in boxes[key]:
             if j not in keysbox and j < len(boxes):
                 keysbox.append(j)
 
     for i in range(len(boxes)):
-        if i not in keysbox: return False
+        if i not in keysbox:
+            return False
 
     return True
