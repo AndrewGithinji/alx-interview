a723df5e0595ba858165b99f8cf3c6f2056d5987
AndrewGithinji <andrewgithinji@hotmail.com>
2023-06-05 11:18:26 +0300

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,29 +1,21 @@
 #!/usr/bin/node
 // Star Wars Characters
-const request = require('request-promise');
+const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-async function getCharacterName(url) {
-try {
-const characterData = await request(url);
-const characterName = JSON.parse(characterData).name;
-console.log(characterName);
-} catch (error) {
-console.log(error);
-}
-}
-
-async function getFilmCharacters(url) {
-try {
-const filmData = await request(url);
-const film = JSON.parse(filmData);
-const characters = film.characters;
-for (const characterUrl of characters) {
-await getCharacterName(characterUrl);
-}
-} catch (error) {
-console.log(error);
-}
-}
-
-getFilmCharacters(url);
+request(url, async (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  for (const cId of JSON.parse(body).characters) {
+    await new Promise((resolve, reject) => {
+      request(cId, (err, response, body) => {
+        if (err) {
+          reject(err);
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
+});
