c3a9b5d2d4e72c50da6f79f0ec3d86adbbf6cd61
AndrewGithinji <andrewgithinji@hotmail.com>
2023-06-05 11:50:40 +0300

    Star Wars Characters mandatory

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,21 @@
 #!/usr/bin/node
+// Star Wars Characters
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api';
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
-if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) {
-      console.log(err);
-    }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      }));
-
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
-  });
-}
+request(url, async (err, response, body) => {
+  if (err) {
+    console.log(err);
+  }
+  for (const cId of JSON.parse(body).characters) {
+    await new Promise((resolve, reject) => {
+      request(cId, (err, response, body) => {
+        if (err) {
+          reject(err);
+        }
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
+});
