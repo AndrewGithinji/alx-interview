2feb7c305230527da0dc6cacbbfc529861733dcf
AndrewGithinji <andrewgithinji@hotmail.com>
2023-06-11 11:34:39 +0300

    Rotate 2D Matrix

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,14 @@
+def rotate_2d_matrix(matrix):
+    N = len(matrix[0])
+    for i in range(N // 2):
+        for j in range(i, N - i - 1):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[N - 1 - j][i]
+            matrix[N - 1 - j][i] = matrix[N - 1 - i][N - 1 - j]
+            matrix[N - 1 - i][N - 1 - j] = matrix[j][N - 1 - i]
+            matrix[j][N - 1 - i] = temp
+    return matrix
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+rotated_matrix = rotate_2d_matrix(matrix)
+print(rotated_matrix)
