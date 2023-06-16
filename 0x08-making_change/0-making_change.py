e31e829a9a6fa7866dc8d490f4d9552e7acf45b1
AndrewGithinji <andrewgithinji@hotmail.com>
2023-06-16 14:31:02 +0300

    Change comes from within

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,38 +7,19 @@ import sys
 
 def makeChange(coins, total):
     """
-    Given a total a pile of coins of different totalalues,
-    determine the fewest number of coins needed to meet a
-    gitotalen amount total.
+    Given a total and a pile of coins of different values,
+    determine the fewest number of coins needed to meet the
+    given amount total.
     """
     if total <= 0:
         return 0
-    res = minCoins(coins, len(coins), total)
-    if res == 0:
-        return -1
-    return res
 
-
-def minCoins(coins, m, total):
-    """
-    find the minimum coins
-    """
-    table = [0 for i in range(total + 1)]
-    table[0] = 0
-
+    table = [0] + [sys.maxsize] * total
     for i in range(1, total + 1):
-        table[i] = sys.maxsize
-
-    for i in range(1, total + 1):
-
-        for j in range(m):
-            if (coins[j] <= i):
-                sub_res = table[i - coins[j]]
-                if (sub_res != sys.maxsize and
-                        sub_res + 1 < table[i]):
+        for coin in coins:
+            if coin <= i:
+                sub_res = table[i - coin]
+                if sub_res != sys.maxsize and sub_res + 1 < table[i]:
                     table[i] = sub_res + 1
 
-    if table[total] == sys.maxsize:
-        return -1
-
-    return table[total]
+    return -1 if table[total] == sys.maxsize else table[total]
